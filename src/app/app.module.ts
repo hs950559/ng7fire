db7308eacd434a91e97dbeca87c2d27f0e93d402
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2019-02-03 12:27:11 +0530

    routing in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Ng7MaterialModule } from './modules/ng7-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import {CourseComponent} from "./course/course.component";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CourseComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    Ng7MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/ng7-material.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import {
+  MatMenuModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+  MatTabsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatInputModule, 
+  MatListModule, 
+  MatPaginatorModule, 
+  MatProgressSpinnerModule, 
+  MatSelectModule, 
+  MatSidenavModule,
+  MatSortModule,
+  MatTableModule,
+  MatToolbarModule
+} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatInputModule, 
+    MatListModule, 
+    MatPaginatorModule, 
+    MatProgressSpinnerModule, 
+    MatSelectModule, 
+    MatSidenavModule,
+    MatSortModule,
+    MatTableModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatInputModule, 
+    MatListModule, 
+    MatPaginatorModule, 
+    MatProgressSpinnerModule, 
+    MatSelectModule, 
+    MatSidenavModule,
+    MatSortModule,
+    MatTableModule,
+    MatToolbarModule
+  ]
+})
+export class Ng7MaterialModule { }
