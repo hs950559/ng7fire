b2b61591005a7ff066d0ba166067ab4cbbf34877
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2019-02-04 15:54:00 +0530

    batch update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,6 +4,7 @@ import {AngularFirestore} from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import { Course } from '../model/course';
+import { of } from 'rxjs';
 
 firebase.initializeApp(environment.firebaseConfig);
 const db = firebase.firestore();
@@ -16,21 +17,25 @@ const db = firebase.firestore();
 export class AboutComponent implements OnInit {
 
   constructor(private db: AngularFirestore) { 
-    this.db.collection('courses')
-      .snapshotChanges()
-      .subscribe(snaps => {
-        const courses: Course[] = snaps.map(snap => {
-          return <Course>{
-            id: snap.payload.doc.id,
-            ...snap.payload.doc.data()
-          };
-        }); 
 
-        console.log(courses);
-      });
+
+
+    
   }
 
   ngOnInit() {
+  }
+
+  batchUpdate() {
+    // Batch Update
+    const c1 = this.db.doc(`courses/AntNnpolMeY08SiNXi0s`).ref;
+    const c2 = this.db.doc(`courses/91dg8ZgunWInkORHd2yK`).ref;
+
+    const batch = this.db.firestore.batch();
+    batch.update(c1, {titles: {description: 'T1'}});
+    batch.update(c2, {titles: {description: 'T2'}});
+
+    batch.commit();
   }
 
 }
